8e0843002cde49ee21d6bf38f235191b130b7a5d
Ting-An Chou <joanne3634@gmail.com>
2015-09-02 11:37:53 +0800

    coupon 衝突修改

---
+++ assets/js/coupon.js
@@ -0,0 +1,28 @@
+function Click0() {
+	removeClass(document.getElementById("control-tab-1"), "active");
+	addClass(document.getElementById("control-tab-0"), "active");
+	document.getElementById("Tab1").style.display = "none";
+    document.getElementById("Tab0").style.display = "block";
+}
+
+function Click1() {
+	removeClass(document.getElementById("control-tab-0"), "active");
+	addClass(document.getElementById("control-tab-1"), "active");
+	document.getElementById("Tab0").style.display = "none";
+    document.getElementById("Tab1").style.display = "block";
+}
+
+function hasClass(ele,cls) {
+  return !!ele.className.match(new RegExp('(\\s|^)'+cls+'(\\s|$)'));
+}
+
+function addClass(ele,cls) {
+  if (!hasClass(ele,cls)) ele.className += " "+cls;
+}
+
+function removeClass(ele,cls) {
+  if (hasClass(ele,cls)) {
+    var reg = new RegExp('(\\s|^)'+cls+'(\\s|$)');
+    ele.className=ele.className.replace(reg,' ');
+  }
+}
