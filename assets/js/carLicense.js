54a4357bb08a356e20535b9fa3a673f7b62fd7fb
HarryFan <harrisfan@eskygo.com>
2015-09-04 10:04:37 +0800

    update touch start

--- assets/js/carLicense.js
+++ assets/js/carLicense.js
@@ -1,18 +1,25 @@
 jQuery(document).ready(function($) {
 
+
     $('select.dropdown').dropdown(); //Start of車牌介面
     $(".ui.dropdown.selection").bind('touchstart click', function(event) {
         console.log($(".default.text").text().length);
         if ($(".default.text").text().length >= 2) {
             $("#Sheng").show();
+            $("#mask").show().css('height', $(window).height());;
         }
         if ($(".default.text").text().length == 1) {
             $("#Sheng").hide();
             $("#Abc").show();
         }
-       
+
     });
 
+    $("#mask").bind('touchstart click', function(event) {
+        $("#Sheng").hide();
+        $("#Abc").hide();
+        $(this).hide();
+    });
 
     $("#Sheng fieldset label").mousedown(function(event) {
         $(this).addClass("active").siblings().removeClass('active');
