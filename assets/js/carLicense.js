b7d717a5d2271701f728f97b749a92b354f7e844
Ting-An Chou <joanne3634@gmail.com>
2015-09-04 15:37:43 +0800

    修改 mobile click 問題
    use jquery mobile vclick 部分

--- assets/js/carLicense.js
+++ assets/js/carLicense.js
@@ -1,9 +1,14 @@
+$.fn.click = function(listener) {
+    return this.each(function() {
+       var $this = $( this );
+       $this.on('vclick', listener);
+    });
+};
+
 jQuery(document).ready(function($) {
 
-
     $('select.dropdown').dropdown(); //Start of車牌介面
-    $(".ui.dropdown.selection").bind('touchstart click', function(event) {
-      
+    $(".ui.dropdown.selection").click(function(event) {
         if ($(".default.text").text().length >= 2) {
             $("#Sheng").show();
             $("#mask").show().css('height', $(window).height());
@@ -15,10 +20,9 @@ jQuery(document).ready(function($) {
             $("#mask").show().css('height', $(window).height());
             return;
         }
-
     });
 
-    $("#mask").bind('touchstart click', function(event) {
+    $("#mask").click(function(event) {
         $("#Sheng").hide();
         $("#Abc").hide();
         $(this).hide();
