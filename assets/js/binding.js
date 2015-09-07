822e6b33249a23133395aab1eec8e4b785177787
HarryFan <harrisfan@eskygo.com>
2015-09-07 12:54:51 +0800

    update binding alert and afterCarLiencse.html

---
+++ assets/js/binding.js
@@ -0,0 +1,12 @@
+function  PhoneAlert(){
+	alert("请输入正确的手机号码");
+}
+function ValidateAlert(){
+	alert("请输入正确的验証码");
+}
+function CarLicenseAlert(){
+	alert("请输入正确的车牌号");
+}
+jQuery(document).ready(function($) {
+//TODO
+});
