d560028e723854f34cb7022d2bdb3fe8247ed44e
HarryFan <harrisfan@eskygo.com>
2015-09-11 14:45:03 +0800

    update dropdown

--- assets/js/carLicense.js
+++ assets/js/carLicense.js
@@ -1,13 +1,13 @@
 $.fn.click = function(listener) {
     return this.each(function() {
-       var $this = $( this );
-       $this.on('vclick', listener);
+        var $this = $(this);
+        $this.on('vclick', listener);
     });
 };
 
 jQuery(document).ready(function($) {
 
-    $('select.dropdown').dropdown(); //Start of車牌介面
+    //$('select.dropdown').dropdown(); //Start of車牌介面
     $(".ui.dropdown.selection").click(function(event) {
         if ($(".default.text").text().length >= 2) {
             $("#Sheng").show();
@@ -33,7 +33,7 @@ jQuery(document).ready(function($) {
         $(".default.text").text($(this).text());
         $("#Sheng").hide();
         $("#Abc").show();
-            // $("#mask").hide();
+        // $("#mask").hide();
     });
 
     $("#Abc fieldset label").mousedown(function(event) {
@@ -42,5 +42,12 @@ jQuery(document).ready(function($) {
         $(".default.text").append(getAbc);
         $("#Abc").hide();
         $("#mask").hide();
-    }); //End of 車牌介面
+    });
+
+    $("#CarNumber,#ValidateCode,#GetCode,#Phone").click(function(event) {
+        $("#Sheng").hide();
+        $("#Abc").hide();
+        $("#mask").hide();
+    });
+    //End of 車牌介面
 });
